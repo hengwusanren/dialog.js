a68bd15d186acabe570a3781c7d4bc794ce64d50
hengwusanren <youchuangqiba@163.com>
2016-04-05 17:45:28 +0800

    upd8: 兼容 ie.

--- README.md
+++ README.md
@@ -1,1 +1,98 @@
-# dialog.js
+# 创建 Dialog
+
+## new
+
+```javascript
+var dialog1 = new Dialogger({
+    content: 'This is a dialog.',
+    stay: 1
+});
+dialog1.show();
+```
+
+`dialog1.main` 是创建的 dom 节点，但不是最外层节点；最外层节点是 `dialog1.wrapper`。
+
+## 工厂
+
+```javascript
+Dialogger.Create({
+    content: 'This is a dialog.',
+    autoShow: 1,
+    position: 2,
+    width: '50%',
+    height: '50%',
+    top: 100
+});
+```
+
+不需要控制返回对象，可以简单这么做。
+
+# 配置选项
+
+## autoShow
+
+创建后自动显示。
+
+## width, height
+
+宽、高，'100px'，'100%'。
+
+## position
+
+1到9的数字，位置对应：
+
+1 2 3
+4 5 6
+7 8 9
+
+常用5，即在屏幕中心显示；2、4、6、8可配合100%的宽或高。
+
+## content
+
+最重要的，`this.main` 的子元素，可以是字符串也可以是 Node。
+
+## stay
+
+是否常驻。默认为否，即 dialog 关闭时从文档中删除；如果常驻则隐藏，不删除，创建时可以用 new。
+
+## top, left ...
+
+用于调整到屏幕边缘的距离。
+
+## zIndex
+
+设置 z-index，以防被遮挡。
+
+## block
+
+是否阻塞页面，如果是，则点击周围空白不关闭。
+
+## noMask
+
+是否要有遮罩，即背后的半透明阴影层。
+
+## ready
+
+dialog 渲染后回调。
+
+## close
+
+dialog 关闭后回调。
+
+## fullscreen
+
+是否全屏显示。
+
+# 接口
+
+## render(content)
+
+渲染，content 可以是字符串或 Node。
+
+## show / hide
+
+显示/隐藏。
+
+## close
+
+关闭。
--- dialog.js
+++ dialog.js
@@ -1,6 +1,54 @@
-/**
+/****************
+ * HACK: 数组 forEach
+ ****************/
+ (function() {
+    if (!Array.prototype.forEach) {
+        Array.prototype.forEach = function(fun /*, thisp*/ ) {
+            var len = this.length;
+            if (typeof fun != "function")
+                throw new TypeError();
+
+            var thisp = arguments[1];
+            for (var i = 0; i < len; i++) {
+                if (i in this)
+                    fun.call(thisp, this[i], i, this);
+            }
+        };
+    }
+})();
+
+/****************
+ * HACK: 函数 bind
+ * 参考 https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+ ****************/
+(function() {
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function(oThis) {
+            if (typeof this !== 'function') {
+                // closest thing possible to the ECMAScript 5
+                // internal IsCallable function
+                throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+            }
+
+            var aArgs = Array.prototype.slice.call(arguments, 1),
+                fToBind = this,
+                fNOP = function() {},
+                fBound = function() {
+                    return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
+                        aArgs.concat(Array.prototype.slice.call(arguments)));
+                };
+
+            fNOP.prototype = this.prototype;
+            fBound.prototype = new fNOP();
+
+            return fBound;
+        };
+    }
+})();
+
+/****************
  * 加载依赖文件
- */
+ ****************/
 (function(filename) {
     var loadExtentFile = function(filename) {
         if (filename instanceof Array) {
@@ -22,16 +70,16 @@
             el.setAttribute('type', 'text/css');
             el.setAttribute('href', filename);
         }
-        document.head.appendChild(el);
+        (document.head || document.getElementsByTagName('head')[0]).appendChild(el);
     };
     loadExtentFile(filename);
 })(['./dialog.css']);
 
 
-/**
+/****************
  * 辅助函数：轻量级模板引擎
  * 参考 http://krasimirtsonev.com/blog/article/Javascript-template-engine-in-just-20-line
- */
+ ****************/
 var templateEngine = function(html, options) {
     var re = /{([^}]+)?}/g,
         reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g,
@@ -55,11 +103,18 @@ var templateEngine = function(html, options) {
     return new Function(code.replace(/[\r\t\n]/g, '')).apply(options);
 };
 
-/**
+/****************
  * 辅助函数：获取元素的样式值
  * 参考 Jquery 作者 John Resig
- */
+ ****************/
 var getStyle = function(el, name) {
+    nameWords = name.split('-');
+    for(var i = 1, len = nameWords.length; i < len; i++) {
+        if(!nameWords[i].length) continue;
+        nameWords[i] = nameWords[i].charAt(0).toUpperCase() + nameWords[i].substr(1);
+    }
+    name = nameWords.join('');
+
     if (el.style[name]) {
         return el.style[name];
     } else if (el.currentStyle) {
@@ -75,9 +130,9 @@ var getStyle = function(el, name) {
 };
 
 
-/**
+/****************
  * Dialog 类
- */
+ ****************/
 var Dialogger = (function(te, gs) {
     var Dialog = function(conf) {
         this.constructor = Dialog;
@@ -99,6 +154,7 @@ var Dialogger = (function(te, gs) {
         this.right = conf.right || 0;
         this.bottom = conf.bottom || 0;
         this.left = conf.left || 0;
+        this.withBorder = !!conf.withBorder; // 是否停靠时也显示边框
 
         // dom 缓存
         this.wrapper = null; // the wrapper node
@@ -116,16 +172,23 @@ var Dialogger = (function(te, gs) {
                 .id);
 
             el.onclick = (function(event) {
-                if (event.target.className.indexOf(
-                        'dialog-wrapper') >= 0 || event
-                    .target.className.indexOf(
+                event = event ? event : window.event;
+
+                var obj = event.srcElement ? event.srcElement : event.target;
+                if (obj.className.indexOf(
+                        'dialog-wrapper') >= 0 || obj.className.indexOf(
                         'dialog-docker') >= 0) {
-                    if (!this.block) this.close();
-                } else if (event.target.className ==
-                    'btn-close') {
+                    if (!this.block) {
+                        this.close();
+                        this.onClose();
+                    }
+                } else if (obj.className.indexOf(
+                    'btn-close') >= 0) {
                     this.close();
                     this.onClose();
                 }
+
+                event.preventDefault ? event.preventDefault() : (event.returnValue = false);
             }).bind(this);
 
             this.onReady(el);
@@ -143,15 +206,14 @@ var Dialogger = (function(te, gs) {
                 this.wrapper = null;
                 var el = document.createElement('div');
                 el.innerHTML = this.constructor.Template;
-                this.wrapper = el.getElementsByClassName(this.constructor
-                    .WrapClassName)[0];
+                this.wrapper = this.constructor.GetElementsByClassName(this.constructor
+                    .WrapClassName, null, el)[0];
                 this.wrapper.id = id;
                 this.wrapper.style.visibility = 'hidden';
                 this.wrapper.style.zIndex = this.zIndex;
-                if (this.noOverlay) this.wrapper.style.backgroundColor =
-                    'rgba(0, 0, 0, 0)';
-                this.main = this.wrapper.getElementsByClassName(
-                    this.constructor.MainClassName)[0];
+                if (this.noOverlay) this.wrapper.className += ' ' + this.wrapper.className + '-noOverlay';
+                this.main = this.constructor.GetElementsByClassName(
+                    this.constructor.MainClassName, null, this.wrapper)[0];
                 document.body.appendChild(this.wrapper);
             }
 
@@ -165,9 +227,17 @@ var Dialogger = (function(te, gs) {
 
             this._setLayout();
             this.wrapper.style.visibility = 'visible';
-            document.body.style.overflow = 'hidden';
+            this._disableScroll();
 
             return this.wrapper;
+        },
+        _disableScroll: function() {
+            document.body.style.overflow = 'hidden';
+            document.documentElement.style.overflow = 'visible';
+        },
+        _restoreScroll: function() {
+            document.body.style.overflow = this._bodyOverflow;
+            document.documentElement.style.overflow = '';
         },
         _setLayout: function(el) {
             if (!el) var el = this.main;
@@ -183,10 +253,9 @@ var Dialogger = (function(te, gs) {
                 el.style.height = this.height;
                 console.log('width: ' + this.width +
                     ', height: ' + this.height);
-                el.parentNode.setAttribute('class', el.parentNode
-                    .getAttribute(
-                        'class') + ' dialog-docker-p' +
-                    this.position);
+                el.parentNode.className = el.parentNode
+                    .className + ' dialog-docker-p' +
+                    this.position;
                 el.parentNode.style.width = this.width;
                 el.parentNode.style.height = this.height;
                 el.style.width = '100%';
@@ -200,50 +269,50 @@ var Dialogger = (function(te, gs) {
                     }
                 }
 
-                var xy = [0, 0];
-                if (this.top && 'number' == typeof this.top) {
-                    xy[1] += this.top;
-                }
-                if (this.right && 'number' == typeof this.right) {
-                    xy[0] -= this.right;
-                }
-                if (this.bottom && 'number' == typeof this.bottom) {
-                    xy[1] -= this.bottom;
-                }
-                if (this.left && 'number' == typeof this.left) {
-                    xy[0] += this.left;
-                }
-
-                this._setOffset(el, null, xy);
+                this._setOffset(el);
             }
         },
-        _setOffset: function(el, ds, xy) {
+        _setOffset: function(el, ds) {
             if (!el) var el = this.main;
-
             if (!ds) var ds = ['top', 'right', 'bottom', 'left'];
-            if (!xy) var xy = [0, 0];
-            ds.forEach(function(d) {
-                var dv = this.constructor.GetStyle(el,
-                    d);
-                if (dv == 'auto') return;
-                var borderWidth = this.constructor.GetStyle(
-                    el, 'border-' + d);
-                if (borderWidth.split(' ') < 3) return;
-                borderWidth = borderWidth.split(' ')[0];
-                if (borderWidth.lastIndexOf('px') !=
-                    borderWidth.length - 2) return;
-                borderWidth = parseFloat(borderWidth.substring(
-                    0, borderWidth.length - 2));
-                if (d == 'top') {
-                    xy[1] -= borderWidth;
-                } else if (d == 'right') {
-                    xy[0] += borderWidth;
-                } else if (d == 'bottom') {
-                    xy[1] += borderWidth;
-                } else if (d == 'left') {
-                    xy[0] -= borderWidth;
-                }
-            }.bind(this));
+            var xy = [0, 0];
+            if (this.top && 'number' == typeof this.top) {
+                xy[1] += this.top;
+            }
+            if (this.right && 'number' == typeof this.right) {
+                xy[0] -= this.right;
+            }
+            if (this.bottom && 'number' == typeof this.bottom) {
+                xy[1] -= this.bottom;
+            }
+            if (this.left && 'number' == typeof this.left) {
+                xy[0] += this.left;
+            }
+
+            if(!this.withBorder) {
+                ds.forEach(function(d) {
+                    var dv = this.constructor.GetStyle(el,
+                        d);
+                    if (dv == 'auto') return;
+                    var borderWidth = this.constructor.GetStyle(
+                        el, 'border-' + d + '-width');
+                    if (borderWidth.split(' ') < 3) return;
+                    borderWidth = borderWidth.split(' ')[0];
+                    if (borderWidth.lastIndexOf('px') !=
+                        borderWidth.length - 2) return;
+                    borderWidth = parseFloat(borderWidth.substring(
+                        0, borderWidth.length - 2));
+                    if (d == 'top') {
+                        xy[1] -= borderWidth;
+                    } else if (d == 'right') {
+                        xy[0] += borderWidth;
+                    } else if (d == 'bottom') {
+                        xy[1] += borderWidth;
+                    } else if (d == 'left') {
+                        xy[0] -= borderWidth;
+                    }
+                }.bind(this));
+            }
 
             var translateVal = 'translate(' + xy[0] + 'px,' +
                 xy[
@@ -253,20 +322,33 @@ var Dialogger = (function(te, gs) {
             el.style.MozTransform = translateVal;
             el.style.OTransform = translateVal;
             el.style.msTransform = translateVal;
+
+            var ieVer = this.constructor.IEVersionCheck();
+            if (ieVer > 0 && ieVer <= 8) {
+                if (ieVer < 8) {
+                    el.setAttribute('style', el.getAttribute('style') + ';filter: progid:DXImageTransform.Microsoft.Matrix(M11=1,M12=0,M21=0,M22=1,SizingMethod=\'auto expand\')');
+                } else {
+                    el.setAttribute('style', el.getAttribute('style') + ';-ms-filter: "progid:DXImageTransform.Microsoft.Matrix(M11=1, M12=0, M21=0, M22=1, SizingMethod=\'auto expand\')"');
+                }
+                if(this.constructor.GetStyle(el, 'top') != 'auto') el.style.marginTop = xy[1] + 'px';
+                else el.style.bottom = -xy[1] + 'px';
+                if(this.constructor.GetStyle(el, 'left') != 'auto') el.style.marginLeft = xy[0] + 'px';
+                else el.style.right = -xy[0] + 'px';
+            }
         },
         show: function() {
             if (!this.wrapper) this.init(); // 如果还没 init 则 init
 
             this.wrapper.style.display = 'block';
-            document.body.style.overflow = 'hidden';
+            this._disableScroll();
         },
         hide: function() {
-            document.body.style.overflow = this._bodyOverflow;
+            this._restoreScroll();
             this.wrapper.style.display = 'none';
         },
         remove: function() {
             this.wrapper.parentNode.removeChild(this.wrapper);
-            document.body.style.overflow = this._bodyOverflow;
+            this._restoreScroll();
             this.main = null;
             this.wrapper = null;
         },
@@ -275,12 +357,89 @@ var Dialogger = (function(te, gs) {
         }
     };
 
-    /**
+    /****************
      * 静态成员
-     */
+     ****************/
     Dialog.WrapClassName = 'dialog-wrapper';
     Dialog.MainClassName = 'dialog';
     Dialog.TemplateEngine = te;
+    /*
+     * getElementsByClassName 兼容
+     * Developed by Robert Nyman, http://www.robertnyman.com
+     * Code/licensing: http://code.google.com/p/getelementsbyclassname/
+     */
+    Dialog.GetElementsByClassName = function(className, tag, elm) {
+        if (document.getElementsByClassName) {
+            _getElementsByClassName = function(className, tag, elm) {
+                elm = elm || document;
+                var elements = elm.getElementsByClassName(className),
+                    nodeName = (tag) ? new RegExp("\\b" + tag + "\\b", "i") : null,
+                    returnElements = [],
+                    current;
+                for (var i = 0, il = elements.length; i < il; i += 1) {
+                    current = elements[i];
+                    if (!nodeName || nodeName.test(current.nodeName)) {
+                        returnElements.push(current);
+                    }
+                }
+                return returnElements;
+            };
+        } else if (document.evaluate) {
+            _getElementsByClassName = function(className, tag, elm) {
+                tag = tag || "*";
+                elm = elm || document;
+                var classes = className.split(" "),
+                    classesToCheck = "",
+                    xhtmlNamespace = "http://www.w3.org/1999/xhtml",
+                    namespaceResolver = (document.documentElement.namespaceURI === xhtmlNamespace) ? xhtmlNamespace : null,
+                    returnElements = [],
+                    elements,
+                    node;
+                for (var j = 0, jl = classes.length; j < jl; j += 1) {
+                    classesToCheck += "[contains(concat(' ', @class, ' '), ' " + classes[j] + " ')]";
+                }
+                try {
+                    elements = document.evaluate(".//" + tag + classesToCheck, elm, namespaceResolver, 0, null);
+                } catch (e) {
+                    elements = document.evaluate(".//" + tag + classesToCheck, elm, null, 0, null);
+                }
+                while ((node = elements.iterateNext())) {
+                    returnElements.push(node);
+                }
+                return returnElements;
+            };
+        } else {
+            _getElementsByClassName = function(className, tag, elm) {
+                tag = tag || "*";
+                elm = elm || document;
+                var classes = className.split(" "),
+                    classesToCheck = [],
+                    elements = (tag === "*" && elm.all) ? elm.all : elm.getElementsByTagName(tag),
+                    current,
+                    returnElements = [],
+                    match;
+                for (var k = 0, kl = classes.length; k < kl; k += 1) {
+                    classesToCheck.push(new RegExp("(^|\\s)" + classes[k] + "(\\s|$)"));
+                }
+                for (var l = 0, ll = elements.length; l < ll; l += 1) {
+                    current = elements[l];
+                    match = false;
+                    for (var m = 0, ml = classesToCheck.length; m < ml; m += 1) {
+                        match = classesToCheck[m].test(current.className);
+                        if (!match) {
+                            break;
+                        }
+                    }
+                    if (match) {
+                        returnElements.push(current);
+                    }
+                }
+                return returnElements;
+            };
+        }
+        return _getElementsByClassName(className, tag, elm);
+    };
+    Dialog.Frame = '<iframe id="{id}" frameBorder=0 scrolling=no width="100%" onLoad="iFrameHeight()"></iframe>';
     Dialog.Template =
         '<div id="{id}" class="dialog-wrapper"><div class="dialog-docker"><div class="dialog">{content}</div></div></div>';
     Dialog.GetStyle = gs;
@@ -291,6 +450,22 @@ var Dialogger = (function(te, gs) {
         (new(this)({
             content: text
         })).show();
+    };
+    Dialog.IEVersionCheck = function() {
+        var userAgent = navigator.userAgent;
+        var ie = (!!window.ActiveXObject || "ActiveXObject" in window) || (userAgent.indexOf("compatible") >= 0 && userAgent.indexOf("MSIE") > -1 && userAgent.indexOf("Opera") < 0);
+        if (!ie) return -1;
+
+        var re = new RegExp("MSIE (\\d+\\.\\d+);");
+        re.test(userAgent);
+        var fIEVersion = parseFloat(RegExp["$1"]);
+        if (fIEVersion < 6) return 5;
+        if (fIEVersion < 7) return 6;
+        if (fIEVersion < 8) return 7;
+        if (fIEVersion < 9) return 8;
+        if (fIEVersion < 10) return 9;
+        if (fIEVersion < 11) return 10;
+        return 11;
     };
 
     return Dialog;
